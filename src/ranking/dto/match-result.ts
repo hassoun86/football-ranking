49bfdafd92e144d37d8d8e1c55fa01480701110e
Hussein <hussein@emil.de>
2022-02-22 17:10:15 +0100

    Fix docker-compose

--- src/ranking/dto/match-result.ts
+++ src/ranking/dto/match-result.ts
@@ -5,13 +5,11 @@ export default class MatchResult {
   @IsInt()
   @ApiProperty({
     description: 'The team id',
-    example: 1,
   })
   teamId: number;
 
   @ApiProperty({
     description: 'Total scored goals',
-    example: 3,
   })
   @IsInt()
   goalsScored: number;
